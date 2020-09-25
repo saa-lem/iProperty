956eaf9909d755034bdc264edff4b2105cac9ea4
Salem Owino <salemowino18@gmail.com>
2020-09-25 13:03:40 +0300

    Deployment

--- salem/settings.py
+++ salem/settings.py
@@ -176,6 +176,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 django_heroku.settings(locals())

a5db81aa89b07ca05144e1988050687803199c0f
Salem Owino <salemowino18@gmail.com>
2020-09-25 13:29:52 +0300

    Deployment

--- salem/settings.py
+++ salem/settings.py
@@ -117,12 +117,12 @@ else:
      }
 }
 # production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
+# else:
+#    DATABASES = {
+#        'default': dj_database_url.config(
+#            default=config('DATABASE_URL')
+#        )
+#    }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)

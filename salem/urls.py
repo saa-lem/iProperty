53c7c769e36d8602b86b6b78b1908237653bfdc2
Salem Owino <salemowino18@gmail.com>
2020-09-25 12:18:18 +0300

    Deployment

--- salem/urls.py
+++ salem/urls.py
@@ -19,5 +19,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('land.urls')),
-    path('', include('django.contrib.auth.urls')),
 ]
